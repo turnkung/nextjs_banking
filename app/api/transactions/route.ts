7d0bd1fc9ea97d2c3f37c42d3aea39a7884d6e50
turnkung <promsith.13@gmail.com>
2024-06-28 21:15:42 +0700

    (Test) Update Rest API

--- app/api/transactions/route.ts
+++ app/api/transactions/route.ts
@@ -1,7 +1,32 @@
+import { getTransactionById } from "@/lib/actions/transaction.actions";
+import { parseStringify } from "@/lib/utils";
 import { NextResponse } from "next/server"
+import { Client, Databases, Query } from "node-appwrite";
 
-export const GET = () => {
+export const GET = async (request: Request) => {
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get('id');
+
+    // const appwriteClient = new Client()
+    //     .setEndpoint("https://cloud.appwrite.io/v1")
+    //     .setProject("6635581a000bb404b5fb");
+
+    // const databases = new Databases(appwriteClient);
+    // const transaction = await databases.listDocuments(
+    //     process.env.APPWRITE_DATABASE_ID!, process.env.APPWRITE_TRANSACTION_COLLECTION_ID!,
+    //     [Query.equal('$id', id!)]
+    // );
+
+    // const transactionData = parseStringify(transaction.documents[0]);
+
+    // return NextResponse.json({
+    //     hello: "world",
+    //     id: id,
+    // });
+
+    const transactionData = await getTransactionById(id!);
+
     return NextResponse.json({
-        hello: "world"
+        transactionData
     });
 }
